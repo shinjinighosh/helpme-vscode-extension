2e738ef560bdc82530a3e14fda6801b42b10bf71
Shinjini Ghosh <shinghos@mit.edu>
2020-06-27 17:51:24 -0400

    change success

--- src/extension.ts
+++ src/extension.ts
@@ -17,7 +17,7 @@ export function activate(context: vscode.ExtensionContext) {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from HelloWorld!');
+		vscode.window.showInformationMessage('Hello VS Code from HelloWorld!');
 	});
 
 	context.subscriptions.push(disposable);
