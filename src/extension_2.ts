d7ba3a3d73e2d50cd4d31d5ee58bfbddedc2e37b
Raul Alcantara <ralcanta@mit.edu>
2020-06-28 02:42:27 -0400

    added inserting question functionality

--- src/extension_2.ts
+++ src/extension_2.ts
@@ -14,41 +14,108 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('helloworld.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+	// let disposable = vscode.commands.registerCommand('helloworld.helloWorld', () => {
+	// 	// The code you place here will be executed every time your command is executed
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello VS Code from another extension again!');
-    });
+	// 	// Display a message box to the user
+	// 	vscode.window.showInformationMessage('Hello VS Code from another extension again!');
+    // });
+    let showBox = vscode.commands.registerCommand("helloworld.showBox", ()=>{
+        showMessageBox();
+    })
+    function showMessageBox(){
+        let questionInputBox = vscode.window.createInputBox();
+        questionInputBox.busy = true;
+        questionInputBox.placeholder = "Put it here!";
+        questionInputBox.title = "What question do you want to add?";
+        questionInputBox.onDidAccept(()=>{
+            let question = questionInputBox.value;
+            questionInputBox.hide();
+
+            let linkInputBox= vscode.window.createInputBox();
+            linkInputBox.title = "What was the link you used?";
+            linkInputBox.placeholder = "Put it also here!";
+            linkInputBox.onDidAccept(()=>{
+                console.log("question: "  + question);
+                console.log("link used: " + linkInputBox.value);
+                linkInputBox.hide();
+            });
+            linkInputBox.ignoreFocusOut = true; //so that when it goes 
+            linkInputBox.show();
+        });
+        questionInputBox.ignoreFocusOut = true;
+        questionInputBox.show();
+    }
+    // vscode.commands.
     let openBrowser = vscode.commands.registerCommand("helloworld.openBrowser", (url)=>{
         // vscode.window.showInformationMessage("openeeed!");
         // vscode.env.openExternal(Uri.parse(url));
         console.log("vscode:"+ vscode.Uri.parse(url));
         vscode.env.openExternal(vscode.Uri.parse(url));
+        
+        showMessageBox();
     })
-    function getQuestion(question: string){ //TODO: maybe point to stackoverflow instead? 
+    function getQuestion(question: string, link: string){ //TODO: maybe point to stackoverflow instead? 
         let browserCompleter = new vscode.CompletionItem(question);
         browserCompleter.kind = vscode.CompletionItemKind.Event
         browserCompleter.command = {
             command: "helloworld.openBrowser",
-            arguments: ["https://www.google.com/search?q="+question],
+            arguments: [link],
             title: "See it on the web...again?" // probably useless idk
         }
         return browserCompleter;
     }
-    const provider1 = vscode.languages.registerCompletionItemProvider('plaintext', {
+    const hack = vscode.languages.registerCompletionItemProvider('plaintext', {
 		provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken, context: vscode.CompletionContext) {
-            let all_questions = [ //TODO: find the right questions
-                "What time is it?",
-                "I am tired",
-                "How much is 2+2?",
-                "lol"
-            ]
-            return all_questions.map(question=>getQuestion(question))
+            let completion = new vscode.CompletionItem("Add a question!");
+            completion.kind = vscode.CompletionItemKind.Event
+            completion.command = {
+                command: "helloworld.showBox",
+                // arguments: [link],
+                title: "See it on the web...again?" // probably useless idk
+            }
+            completion.insertText = ""; //dont insert any text
+            return [completion]
+        }
+        }, "+");
+    const searcher = vscode.languages.registerCompletionItemProvider('plaintext', {
+		provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken, context: vscode.CompletionContext) {
+            let INFO = { //TODO: find the right questions
+                description: "This contains all questions and their respective links",
+                all_questions: [
+                    {
+                       display_name: "What time is it with python",
+                       link:  "https://www.google.com/search?q=What time is it with python",
+                       keywords: [
+                           "time", 
+                           "question"
+                       ]
+                    },
+                    {
+                        display_name: "How much is 2 plus 2?",
+                        link: "https://www.google.com/search?q=How much is 2 plus 2",
+                        keywords:[
+                            "add",
+                            "sum",
+                            "+",
+                            "2+2"
+                        ]
+                    }
+                ]
+            }
+            return INFO.all_questions.map(
+                question => getQuestion(question.display_name, question.link)
+                )
 		}
-	}, "#");
-
-	context.subscriptions.push(disposable, provider1, openBrowser);
+    }, "#");
+    // vscode.commands.registerCommand
+    const finder = vscode.commands.registerCommand("helloworld.helloWorld",
+    ()=>{
+        // console.log("ahhh");
+        // let value = vscode.window.showInputBox();
+        // console.log("This is what was inputted:" + value);
+    })
+	context.subscriptions.push(finder, searcher, openBrowser, hack, showBox);
 }
 
 // this method is called when your extension is deactivated
